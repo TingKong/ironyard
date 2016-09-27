88a86a1c3b3be3090d9a27f3b16c6310a468d963
Ting <kongsau@gmail.com>
2016-09-27 15:23:09 -0400

    removing commented out section

--- week1/fizzbuzz_exercise/fizzbuzz_exercise/Program.cs
+++ week1/fizzbuzz_exercise/fizzbuzz_exercise/Program.cs
@@ -86,30 +86,7 @@ namespace fizzbuzz_exercise
                 }
             }
 
-            //for (int i = 0; i < newNum; ++i)
-            //{
-            //    if (i <= 0)
-            //    {
-            //        Console.WriteLine("0");
-            //    }
-            //    else if (i % 5 == 0 && i % 3 == 0)
-            //    {
-            //        Console.WriteLine("FuzzBuzz");
-            //    }
-
-            //    else if (i % 3 == 0)
-            //    {
-            //        Console.WriteLine("Fizz");
-            //    }
-            //    else if (i % 5 == 0)
-            //    {
-            //        Console.WriteLine("Buzz");
-            //    }
-
-            //    else
-            //    {
-            //        Console.WriteLine(i);
-            //    }
+          
 
 
 
