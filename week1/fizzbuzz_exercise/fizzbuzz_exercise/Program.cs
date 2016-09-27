b1988440459f1ee2339f86195d6703b00719a9c0
Ting <kongsau@gmail.com>
2016-09-27 13:41:08 -0400

    in progress save

---
+++ week1/fizzbuzz_exercise/fizzbuzz_exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numarray = new int[100];
+            for (int i = 0; i < numarray.Length; ++i)
+            {
+               numarray[i] = i + 1;
+
+                if (numarray[i] % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+               else if (numarray[i] % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else (numarray[i] % 3 == 0 && numarray[i] % 5 == 0)
+                {
+                    Console.WriteLine("FuzzBuzz");
+                }
+
+                Console.WriteLine(numarray[i]);
+
+            }
+
+           
+
+            Console.Read();
+
+        }
+    }
+}
--- week1/fizzbuzz_exercise/fizzbuzz_exercise/Properties/AssemblyInfo.cs
+++ week1/fizzbuzz_exercise/fizzbuzz_exercise/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("fizzbuzz_exercise")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("fizzbuzz_exercise")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("04a1b797-bf1f-439b-986a-25d67c74c4e5")]
+[assembly: Guid("7906ee5c-310e-4afb-9aff-7c6e8eb16051")]
 
 // Version information for an assembly consists of the following four values:
 //
