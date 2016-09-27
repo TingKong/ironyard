a8098555c027186a748dd11280b645587f58217a
Ting <kongsau@gmail.com>
2016-09-27 14:39:03 -0400

    Finished array answer, in progress input answer

--- week1/fizzbuzz_exercise/fizzbuzz_exercise/Program.cs
+++ week1/fizzbuzz_exercise/fizzbuzz_exercise/Program.cs
@@ -11,32 +11,84 @@ namespace fizzbuzz_exercise
         static void Main(string[] args)
         {
 
-            int[] numarray = new int[100];
-            for (int i = 0; i < numarray.Length; ++i)
-            {
-               numarray[i] = i + 1;
+            //ANSWER IN ARRAY
+            //for (int i = 0; i < 100; ++i)
+            //{
+            //    if (i <= 0)
+            //    {
+            //        Console.WriteLine("0");
+            //    }
+            //    else if (i % 5 == 0 && i % 3 == 0)
+            //    {
+            //        Console.WriteLine("FuzzBuzz");
+            //    }
 
-                if (numarray[i] % 3 == 0)
+            //    else if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //ANSWER IN INPUT
+
+            //take input
+            //spit up number up to the input
+            //if divisible by 3 = Fizz
+            //if divisible by 5 = Buzz
+            //if divisible by both = FizzBuzz
+
+
+            string y;
+
+            Console.WriteLine("How high do you want to go?");
+
+            y = Console.ReadLine();
+            int newNum;
+            if (!Int32.TryParse(y, out newNum))
+            {
+                Console.WriteLine("you didn't enter a number");
+
+            }
+            for (int i = 0; i < newNum; ++i)
+            {
+                if (i <= 0)
                 {
-                    Console.WriteLine("Fizz");
+                    Console.WriteLine("0");
                 }
-               else if (numarray[i] % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else (numarray[i] % 3 == 0 && numarray[i] % 5 == 0)
+                else if (i % 5 == 0 && i % 3 == 0)
                 {
                     Console.WriteLine("FuzzBuzz");
                 }
 
-                Console.WriteLine(numarray[i]);
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+
+
+                Console.Read();
 
             }
-
-           
-
-            Console.Read();
-
         }
     }
+
 }
