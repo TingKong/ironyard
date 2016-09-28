112c712d52f7b38f2dac4b8c626f5b44cea8c011
Ting <kongsau@gmail.com>
2016-09-28 15:10:51 -0400

    tictactoe in progress

---
+++ week1/tictacktoe/tictacktoe/tictacktoe/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tictacktoe
+{
+    class Program
+    {
+        private static object i;
+
+        static void Main(string[] args)
+        {
+            //create a grid
+            //assign player 1 - 0
+            //assign player 2 - X
+            //ask player 1 to assign X to a place
+            //check if the position is placed
+            //check winner
+            //create a grid
+            //ask player 2 to assign 0
+            //check for a winner
+            //loop until there is a winner
+            //if there is no winner then it is a tie
+
+
+            
+         
+            char player1 = 'X';
+            char player2 = 'O';
+
+            int[] myArray = new int[9];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i;
+
+            }
+
+            WriteGridLine1(string.Format(" {0} | {1} | {2} ", i, i, i));
+            Console.WriteLine("____________");
+            WriteGridLine1(string.Format(" {0} | {1} | {2} ", i, i, i));
+            Console.WriteLine("____________");
+            WriteGridLine1(string.Format(" {0} | {1} | {2} ", i, i, i));
+
+
+
+
+          
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+        static string WriteGridLine1(string lines)
+        {
+            Console.WriteLine(lines);
+            return lines;
+           
+        }
+
+
+    }
+}
