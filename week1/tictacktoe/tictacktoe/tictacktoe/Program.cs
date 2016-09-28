3376d2cdbb04117af606fa030d2764075569ee88
Ting <kongsau@gmail.com>
2016-09-28 17:06:41 -0400

    created a method for question and input for player1

--- week1/tictacktoe/tictacktoe/tictacktoe/Program.cs
+++ week1/tictacktoe/tictacktoe/tictacktoe/Program.cs
@@ -16,39 +16,47 @@ namespace tictacktoe
             //assign player 1 - 0
             //assign player 2 - X
             //ask player 1 to assign X to a place
-            //check if the position is placed
+            //check if the position has an X or O in it
             //check winner
             //create a grid
             //ask player 2 to assign 0
-            //check for a winner
+            //check if the position has an X or O in it
+            //check winner
             //loop until there is a winner
             //if there is no winner then it is a tie
 
 
-            
-         
+
+
             char cross = 'X';
             char circle = 'O';
 
-            int[] myArray = new int[9];
+            string[] myArray = new string[9];
             for (int i = 0; i < myArray.Length; i++)
             {
-                myArray[i] = i;
+                myArray[i] = i.ToString();
 
             }
 
-            Grid( myArray);
+            Grid(myArray);
 
 
-/***converting input into the location***/
-            Console.WriteLine("Select a number");
-            string player1 = Console.ReadLine();
+            /***converting input into the location***/
+            FirstPlayer(myArray, cross);
 
-            if (player1 == "1")
+            Console.WriteLine("Player 2 Select a number");
+            string player2 = Console.ReadLine();
+            int b;
+            Int32.TryParse(player2, out b);
+
+            myArray[b] = circle.ToString();
+            Grid(myArray);
+
+
+            /***check for a winner***/
+            while (myArray[0] == myArray[1] && myArray[1] == myArray[2])
             {
-                int a = 1;
-                Int32.TryParse(player1, out a);
-                myArray[1] == cross;
+                Console.WriteLine("You Won");
 
             }
 
@@ -57,13 +65,25 @@ namespace tictacktoe
             Console.ReadLine();
 
         }
-        static void Grid(int[] myArray)
+        static void Grid(string[] myArray)
         {
             Console.WriteLine(string.Format(" {0} | {1} | {2} ", myArray[0], myArray[1], myArray[2]));
             Console.WriteLine("____________");
             Console.WriteLine(string.Format(" {0} | {1} | {2} ", myArray[3], myArray[4], myArray[5]));
             Console.WriteLine("____________");
             Console.WriteLine(string.Format(" {0} | {1} | {2} ", myArray[6], myArray[7], myArray[8]));
+
+        }
+
+        static void FirstPlayer(string[] myArray2, char smiles)
+        {
+            Console.WriteLine("Player 1 Select a number");
+            string player1 = Console.ReadLine();
+            int a;
+            Int32.TryParse(player1, out a);
+
+            myArray2[a] = smiles.ToString();
+            Grid(myArray2);
 
         }
 
