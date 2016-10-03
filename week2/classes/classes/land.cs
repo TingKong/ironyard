4da838297007fd5bfb1f417ce8c933e965ef070e
Ting <kongsau@gmail.com>
2016-10-03 14:14:56 -0400

    in progress - third class

---
+++ week2/classes/classes/land.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class land
+    {
+        private bool  disneyWorld;
+        private bool  humans;
+        private bool pollution;
+        private int spaceStations;
+        private int godzilla;
+
+        public bool DisneyWorld
+
+        {
+            get
+            {
+                return disneyWorld;
+            }
+            set
+            {
+                disneyWorld = value;
+            }
+        }
+
+        public bool Humans
+
+        {
+            get
+            {
+                return humans;
+            }
+            set
+            {
+                humans = value;
+            }
+        }
+
+        public bool Pollution
+
+        {
+            get
+            {
+                return pollution;
+            }
+            
+        }
+
+        public int SpaceStations
+        {
+            get
+            {
+                return spaceStations;
+            }
+
+            set
+            {
+                if (value < 1)
+                {
+                    spaceStations = 1;
+                }
+                else
+                {
+                    spaceStations = value;
+                }
+            }
+        }
+
+        public int Godzilla
+        {
+            get
+            {
+                return godzilla;
+            }
+
+            set
+            {
+                if (value < 1)
+                {
+                    godzilla = 1;
+                }
+                else
+                {
+                    godzilla; = value;
+                }
+            }
+        }
+    }
+}
---
+++ week2/classes/classes/ocean.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class ocean
+    {
+
+        private bool megalodons = true;
+        private int warShips;
+        private bool pollution;
+        private int giantOctopus;
+        private bool sinkenShips;
+
+
+
+        public bool Megalodons
+
+        {
+            get
+            {
+                return megalodons;
+            }
+            set
+            {
+                megalodons = value;
+            }
+        }
+
+        public int WarShips
+        {
+            get
+            {
+                return warShips;
+            }
+
+            set
+            {
+                if (value < 1)
+                {
+                    warShips = 1;
+                }
+                else
+                {
+                    warShips = value;
+                }
+            }
+        }
+
+        public bool Pollution
+
+        {
+            get
+            {
+                return pollution;
+            }
+        }
+
+        public int GiantOctopus
+        {
+            get
+            {
+                return giantOctopus;
+            }
+
+            set
+            {
+                if (value < 1)
+                {
+                    giantOctopus = 1;
+                }
+                else
+                {
+                    giantOctopus = value;
+                }
+            }
+        }
+
+        public bool SinkenShips
+
+        {
+            get
+            {
+                return sinkenShips;
+            }
+            set
+            {
+               sinkenShips = value;
+            }
+        }
+
+    }
+}
